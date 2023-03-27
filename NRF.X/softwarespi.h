e0fc9f00d8da9f1a5c8eb7924065ed321605b290
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2023-03-27 22:31:47 +0200

    working

---
+++ NRF.X/softwarespi.h
@@ -0,0 +1,30 @@
+/* 
+ * File:   softwarespi.h
+ * Author: marti
+ *
+ * Created on March 27, 2023, 10:29 PM
+ */
+
+#ifndef SOFTWARESPI_H
+#define	SOFTWARESPI_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+    void SPI_init_master(){
+        // Data transmit on rising edge of clock
+        
+    }
+
+    uint8_t SPI_write(uint8_t data){
+
+    }
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SOFTWARESPI_H */
+
